3ccd8e39772f397822ce5ea00c8047aa6fcff0b4
meng <wulalameng@gmail.com>
2017-09-18 11:35:14 -0400

    implement messager using pubsub lib

---
+++ publish-subscribe/messager.js
@@ -0,0 +1,12 @@
+function messager(topic, data) {
+  console.log(`${topic}: ${data}`);
+}
+
+const messageToken = pubsub.subscribe('inbox/NewMessage', messager);
+
+pubsub.publish('inbox/NewMessage', 'hello');
+pubsub.publish('inbox/NewMessage', 'world');
+pubsub.publish('inbox/OldMessage', 'world');
+
+pubsub.unsubscribe(messageToken);
+pubsub.publish('inbox/NewMessage', 'Nope');
